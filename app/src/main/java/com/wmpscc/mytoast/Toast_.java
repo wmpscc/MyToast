26d891cb4900d4bc944f784572bc3b71a8b53393
wmpscc <1214490813@qq.com>
2017-10-09 18:45:58 +0800

    增加图片toast

--- app/src/main/java/com/wmpscc/mytoast/Toast_.java
+++ app/src/main/java/com/wmpscc/mytoast/Toast_.java
@@ -1,8 +1,10 @@
 package com.wmpscc.mytoast;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,6 +25,23 @@ public class Toast_ {
 
     public static Toast_ makeText(Context context, CharSequence  text, int duration) {
         return new Toast_(context, text, duration);
+    }
+
+    public static Toast_ makeText(Context context,CharSequence text,int duration,Drawable imageurl){
+
+        return new Toast_(context,text,duration,imageurl);
+    }
+
+    public Toast_(Context context,CharSequence text,int duration,Drawable imageurl){
+        View v=LayoutInflater.from(context).inflate(R.layout.toast_text_image,null);
+        TextView tv=v.findViewById(R.id.toast_text_image_tv);
+        tv.setText(text);
+        ImageView iv=v.findViewById(R.id.toast_text_image_iv);
+        iv.setImageDrawable(imageurl);
+        mToast=new Toast(context);
+        mToast.setDuration(duration);
+        mToast.setView(v);
+
     }
     public void show() {
         if (mToast != null) {
